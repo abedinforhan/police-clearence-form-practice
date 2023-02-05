ed6b51a22853b4a3c4f6a3e940b9c94ae3cc54b0
asifgenius <asif15-11288@diu.edu.bd>
2023-02-05 18:43:55 +0600

    multer file

--- app.js
+++ app.js
@@ -1,23 +1,89 @@
-const express=require('express')
+const express = require('express')
 const ejs = require('ejs');
 const multer = require('multer')
 const fileUpload = require('express-fileupload');
-const app=express()
-const path =require('path')
+const app = express()
+const path = require('path')
+const { files } = require('./upload')
 //set the view engine to ejs
-app.set('view engine','ejs')
-app.use(express.urlencoded({extended:true}))
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true }))
 app.use(fileUpload())
-app.use(express.static(path.join(__dirname,'public')))
-app.get('/',(req,res,next)=>{
-    res.render('index')
+app.use(express.static(path.join(__dirname, 'public')))
+app.get('/', (req, res, next) => {
+  res.render('index', {
+  })
 })
-
-app.post('/file',(req,res)=>{
-  console.log('file', req.files.file)
+app.post('/from', (req, res) => {
+  res.render('from.ejs', {
+    goAbroad: req.body.goAbroad,
+    countryofTravel: req.body.countryofTravel,
+    passportNo: req.body.passportNo,
+    issuingCountry: req.body.issuingCountry,
+    issuingDate: req.body.issuingDate,
+    issuingPlace: req.body.issuingPlace,
+    expiryDate: req.body.expiryDate,
+    mobileNo: req.body.mobileNo,
+    email: req.body.email,
+    Nid: req.body.Nid,
+    fatherName: req.body.fatherName,
+    fatherHusbandname: req.body.fatherHusbandname,
+    relation:req.body.relation,
+    motherName: req.body.motherName,
+    dateBrith: req.body.dateBrith,
+    solutation: req.body.solutation
+  })
+  console.log('file', req.files)
+  console.log('passportNo', req.body.passportNo)
+  console.log('goAbroad', req.body.goAbroad)
+  console.log('countryofTravel', req.body.countryofTravel)
+  const nidFile = files('./public/nid').single('nid');
+  nidFile(req, res, (err) => {
+    const { file } = req;
+    console.log('nidfile', file);
+    // if(err){
+    //   res.render('index',{
+    //     massage: err
+    //   })
+    // }
+    // else{
+    //   if(req.file== undefined){
+    //     res.render('index',{
+    //       massage: "No select Image"
+    //     })
+    //   }
+    //   else{
+    //     res.render('index',{
+    //       file
+    //     })
+    //   }
+    // }
+  })
+  const uploadFile = files('./public/image').single('image');
+  uploadFile(req, res, (err) => {
+    const { file } = req;
+    console.log('file', file);
+    // if(err){
+    //   res.render('index',{
+    //     massage: err
+    //   })
+    // }
+    // else{
+    //   if(req.file== undefined){
+    //     res.render('index',{
+    //       massage: "No select Image"
+    //     })
+    //   }
+    //   else{
+    //     res.render('index',{
+    //       file
+    //     })
+    //   }
+    // }
+  })
 })
-const port=5000 || process.env.PORT
-app.listen(port,()=>{
-  console.log(`Listening on ${port}` );
+const port = 5000 || process.env.PORT
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
 })
 
--- upload.js
+++ upload.js
@@ -1,2 +1,44 @@
+const express = require('express')
+const app = express()
 const multer = require('multer')
+const path = require('path')
+const { extname } = require('path');
+const fileUpload = (destination) => {
+	const stroage = multer.diskStorage({
+		destination: function (req, file, cb) {
+			cb(null, destination)
+		},
+		filename: function (req, file, cb) {
+			cb(null, Date.now() + path.extname(file.originalname))
+		}
+	})
+	return multer({
+		storage: stroage,
+		limits: { fileSize: 1000000 },
+		fileFilter: function (req, file, cb) {
+			fileType(file, cb)
+		}
+	})
+}
+function fileType(file, cb) {
+	const fileType = /jpeg|jpg|png|gif/;
+	const extname = fileType.test(path.extname(file.originalname).toLowerCase());
+	const mimetype = fileType.test(file.mimetype);
+	if (extname && mimetype) {
+		return cb(null, true)
+	}
+	else {
+		cb('Only Image')
+	}
+}
+
+module.exports = { fileUpload };
+
+
+
+
+
+
+
+
 
